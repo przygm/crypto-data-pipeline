d9d3092cf38b653a3506ec9e764ed8191e8faf6c
Przemysław Zygmunt <p.zygmunt@interia.pl>
2026-04-14 14:50:10 +0000

    stg_crypto and crypto_prices

---
+++ models/marts/crypto_prices.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='incremental',
+    unique_key='day',
+    on_schema_change='append_new_columns'
+) }}
+
+WITH source_data AS (
+
+    SELECT *
+    FROM {{ ref('stg_crypto') }}
+
+    {% if is_incremental() %}
+    WHERE DATE(ingestion_time) >= (
+        SELECT MAX(day) FROM {{ this }}
+    )
+    {% endif %}
+
+)
+
+SELECT
+    DATE(ingestion_time) AS day,
+    COUNT(*) AS row_count,
+    AVG(btc_price) AS avg_btc_price,
+    MAX(btc_price) AS max_btc_price,
+    MIN(btc_price) AS min_btc_price
+FROM source_data
+GROUP BY 1
